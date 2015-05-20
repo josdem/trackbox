27775655b6576111781d8403711b6f439847127a
josdem <joseluis.delacruz@gmail.com>
2015-05-20 16:04:12 -0500

    Adding project domain

---
+++ grails-app/domain/com/makingdevs/Project.groovy
@@ -0,0 +1,16 @@
+package com.makingdevs
+
+class Project {
+  String owner
+  String codeName
+  String description
+  Boolean publicVisible
+
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+
+  }
+
+}
